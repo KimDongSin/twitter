1e7bbfdd072a796286bf3b18dc2d39362705231d
KimDongSin <asstaff2020@gmail.com>
2024-06-09 10:22:14 +0900

    firebase Authentication

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB0JM5e_88v6uhJ27NVqcW4sXb6NIMQJTs",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
